df1fdf8abea6d15dbe2278a0f80088300056ee66
mrtuls1 <mertulas753951@gmail.com>
2021-08-17 20:46:37 +0300

    Initial commit

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+namespace MvcStok.Controllers
+{
+    
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDBStokEntities db = new MvcDBStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]  //eger islem yapılmazsa boş dödürülrse çalıştırılacak alan
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost] // gönderme işlemi yapılırsa çalışacak alan
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("YeniKategori"); 
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+   
+    public class SatisController : Controller
+    {
+        MvcDBStokEntities db = new MvcDBStokEntities();
+        // GET: Satis
+        public ActionResult Index()
+        {
+            var degerler = db.TBLSATISLAR.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLMUSTERILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.MUSTERIAD,
+                                                 Value = i.MUSTERID.ToString()
+                                             }).ToList();
+            ViewBag.mstrad = degerler;
+            List<SelectListItem> degerler1 = (from i in db.TBLURUNLER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.URUNAD,
+                                                 Value = i.URUNID.ToString()
+                                             }).ToList();
+            ViewBag.stsmrk = degerler1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p1)
+        {
+            db.TBLSATISLAR.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Sil(int id)
+        {
+            var satis = db.TBLSATISLAR.Find(id);
+            db.TBLSATISLAR.Remove(satis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GuncellemeEkrani(int id)
+        {
+            var satis = db.TBLSATISLAR.Find(id);
+            List<SelectListItem> degerler = (from i in db.TBLMUSTERILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.MUSTERIAD,
+                                                 Value = i.MUSTERID.ToString()
+                                             }).ToList();
+            ViewBag.mstrad = degerler;
+
+            return View("GuncellemeEkrani",satis);
+        }
+        public ActionResult Guncelle(TBLSATISLAR p1)
+        {
+            var satis = db.TBLSATISLAR.Find(p1.SATISIID);
+            var mstrad = db.TBLMUSTERILER.Where(m => m.MUSTERID == p1.TBLMUSTERILER.MUSTERID).FirstOrDefault();
+            satis.MUSTERI = mstrad.MUSTERID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+    
+}
